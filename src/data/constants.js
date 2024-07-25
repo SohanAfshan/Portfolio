af4bd3a519166c0949a0acac11fa20d00bf22e77
Sk Sohan Afshan <sohanafshan1@gmail.com>
2024-07-25 17:27:10 +0530

    First Commit

---
+++ src/data/constants.js
@@ -0,0 +1,208 @@
+import Image1 from "../assets/Image1.jpeg";
+import Image2 from "../assets/Image2.jpeg";
+import Image3 from "../assets/Image3.jpeg";
+import Image4 from "../assets/Image4.jpeg";
+import Project1 from "../assets/Project1.jpg";
+import Project2 from "../assets/Project2.png";
+
+
+export const Bio = {
+  name: "SK Sohan Afshan",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/SohanAfshan",
+  resume:
+    "https://app.enhancv.com/share/e2dec6f9/?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic",
+  insta: "https://www.instagram.com/the_sohanafshan/",
+  facebook:"https://m.facebook.com/profile.php?id=61559510414648"
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      
+     
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+     
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      
+      
+      {
+        name: "Figma",
+        image:
+          "https://w7.pngwing.com/pngs/911/515/png-transparent-figma-logo-brand-logos-brands-in-colors-icon-thumbnail.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 1,
+    title: "National Science Olympiad",
+    img: "https://d3q0c4wmynnr7l.cloudfront.net/sof_logo-2.jpg",
+    date: "November 2012",
+    desc: "Acquired International Rank of 117 in 15th National Science Olympiad",
+    doc: Image1,
+  },
+  {
+    id: 2,
+    title: "International Maths Olympiad ",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/IMO_logo.svg/330px-IMO_logo.svg.png",
+    date: "February 2015",
+    desc: "Acquired International Rank of 2589 in the finals of 8th International Math Olympiad.",
+    doc: Image2,
+  },
+  {
+    id: 3,
+    title: "All India full contact Karate(Kyokushin Kai) ",
+    img: "https://aikkf.com/images/aikk.png",
+    date: "March 2015",
+    desc: "Acquired Bronze Medal(3rd) in 17th All India Full Contact Karate.",
+    doc: Image3,
+  },
+  {
+    id: 4,
+    title: "Cultural and Recitation Competition ",
+    img: "https://scontent.fccu3-1.fna.fbcdn.net/v/t39.30808-6/310970143_417377843881377_2772576090458322196_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=blw47Od859kQ7kNvgGh_Ta7&_nc_ht=scontent.fccu3-1.fna&oh=00_AYDSxTi6zh1CENcn0WAcSWawXgTDF2wQQJORW2WNDDgy1g&oe=66A6C3EA",
+    date: "Aug 2011",
+    desc: "Acquired 1st Position in recitation in 36th Cultural Competition of Sarat Memorial Institute.",
+    doc: Image4,
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://msit.edu.in/assets/images/logo_msit.png",
+    school: "Meghnad Saha Institute of Technology, Kolkata",
+    date: "2021 - 2025",
+    grade: "8.00 CGPA (Last appeared semester with result",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology at Meghnad Saha Institute of  Technology, Kolkata. I have completed 6 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Information Technology.",
+  },
+  {
+    id: 1,
+    img: "https://www.southpoint.ac.in/wp-content/uploads/2024/06/Logo_W.png",
+    school: "South Point High School",
+    date: "2021",
+    grade: "87.4%",
+    desc: "I completed my higher secondary education at South Point High School, Kolkata.",
+    degree: "CBSE(XII), Science with Biology",
+  },
+  {
+    id: 2,
+    img: "https://www.southpoint.ac.in/wp-content/uploads/2024/06/Logo_W.png",
+    school: "South Point High School, Kolkata",
+    date: "2021",
+    grade: "93.6%",
+    desc: "I completed my higher education at South Point High School,Kolkata.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+ 
+  
+  
+  {
+    id: 1,
+    title: "campusXchange",
+    date: "Dec'2023-Jan'24",
+    description:
+      "A website solely created for the purpose of college students to post and sell their products which are no longer required by them. such as books and equipments for other fellow students. The goal of the project was to ensure that students connect among themselves and buy the products at lower prices rather than visiting stores.",
+    image:Project1,
+    tags: ["MongoDb", "Tailwind CSS", "HTML", "Node JS", "TypeScript", "React JS"],
+    category: "Website Developement",
+    github: "https://github.com/SohanAfshan/campusXchange.git",
+    
+   },
+   {
+    id: 2,
+    title: "Personal Portfolio",
+    date: "Mar 2024",
+    description:
+      "Developed a professional portfolio website to showcase skills, projects, and achievements effectively. Designed with a clean and intuitive interface, ensuring a seamless user experience. ",
+    image:Project2,
+    tags: ["JavaScript", "React JS"],
+    category: "Website Developement",
+    github: "https://github.com/SohanAfshan/campusXchange.git",
+    
+   },
+];
+
+
